b9ee090a866c2f6a8643e9a3f18cc3e4c3ef0ab1
Abdullah Anwar <166462645+absoft-solution@users.noreply.github.com>
2024-10-15 16:59:37 +0500

    COMMIT

--- lib/view/screen/auth/registration/sign_up_screen.dart
+++ lib/view/screen/auth/registration/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,17 +10,14 @@ class SignUpScreen extends StatelessWidget {
   final AuthService authService = Get.put(AuthService());
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
 
   void _signUp() async {
     final email = emailController.text;
     final password = passwordController.text;
     User? user = await authService.signUp(email, password);
 
-    if (user != null) {
-      Get.snackbar('Verification Email Sent',
-          'Please check your email to verify your account.');
-      Get.to(() => LoginScreen());
-    }
   }
 
   @override
@@ -42,10 +40,30 @@ class SignUpScreen extends StatelessWidget {
               decoration: const InputDecoration(labelText: 'Password'),
             ),
             const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _signUp,
-              child: const Text('Sign Up'),
-            ),
+            ElevatedButton(onPressed: (){
+              _signUp();
+              User? user = FirebaseAuth.instance.currentUser;
+              String id =
+              DateTime.now().microsecondsSinceEpoch.toString();
+              firestore.collection('users').doc(id).set({
+                'email': user?.email.toString(),
+                 "isVerified":false ,
+
+              }).then((value) {
+                Get.snackbar(
+                    "Verification Required", "Please Verify your account");
+
+              }).catchError((error) {
+                Get.snackbar("Not Added to Verification List", error.toString());
+              });
+
+
+              if (user != null) {
+                Get.snackbar('Verification Email Sent',
+                    'Please check your email to verify your account.');
+                Get.to(() => LoginScreen());
+              }
+            }, child: Text("Sign Up")),
             TextButton(
               onPressed: () {
                 Get.back();
